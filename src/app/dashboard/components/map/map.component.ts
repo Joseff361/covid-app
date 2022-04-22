d495e495758b8d4c06c7b3f12b49501230f731d9
Joseff361 <Joseff361@outlook.com>
2022-04-22 17:19:55 -0500

    Add map component

--- src/app/dashboard/components/map/map.component.ts
+++ src/app/dashboard/components/map/map.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+import { CovidService } from '../../services/covid.service';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+  styleUrls: ['./map.component.scss'],
 })
 export class MapComponent implements OnInit {
+  private map!: L.Map;
+  public longitude: number = 39.8282;
+  public latitude: number = -98.5795;
 
-  constructor() { }
+  constructor(private covidService: CovidService) {}
 
-  ngOnInit(): void {
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [this.latitude, this.longitude],
+      zoom: 4.5,
+    });
+
+    const tiles = L.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        maxZoom: 18,
+        minZoom: 3,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }
+    );
+
+    tiles.addTo(this.map);
+
+    L.marker([39.8282, -98.5795]).addTo(this.map);
   }
 
+  ngOnInit(): void {
+    this.covidService.getCountryData('peru').subscribe((data) => {
+      this.latitude = Number(data[0].Lat);
+      this.longitude = Number(data[0].Lon);
+
+      this.map.setView([this.latitude, this.longitude], 4.5);
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
 }
